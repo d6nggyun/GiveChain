34bba4cc6ac2dab71991b07d47587d06b75d7011
d6nggyun <d6nggyun@gmail.com>
2025-11-18 14:08:33 +0900

    ⚙️  setting: 백엔드/프론트엔드 초기 파일 세팅

---
+++ backend/src/main/java/blockchain/GiveChain/GiveChainApplication.java
@@ -0,0 +1,13 @@
+package blockchain.GiveChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveChainApplication.class, args);
+	}
+
+}
