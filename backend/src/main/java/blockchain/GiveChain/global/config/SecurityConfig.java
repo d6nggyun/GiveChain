a55c85d9ec1da1eba10214e6619409dff91f158d
d6nggyun <d6nggyun@gmail.com>
2025-11-20 16:33:44 +0900

    ✨ feat: Config 기본 세팅 - #1

---
+++ backend/src/main/java/blockchain/GiveChain/global/config/SecurityConfig.java
@@ -0,0 +1,107 @@
+package blockchain.GiveChain.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
+                .httpBasic(AbstractHttpConfigurer::disable)
+
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers(AUTH_URIS).permitAll()
+                        .requestMatchers(SWAGGER_URIS).permitAll()
+                        .requestMatchers(IMAGE_URIS).permitAll()
+                        .requestMatchers(HttpMethod.POST, UPLOAD_IMAGE_URIS).hasRole("ADMIN")
+                        .requestMatchers(ADMIN_URIS).hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+
+                .exceptionHandling(exceptioin -> exceptioin
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    private static final String[] AUTH_URIS = {
+            "/api/v1/auth/signup",
+            "/api/v1/auth/login",
+            "/api/v1/auth/login/test",
+            "/api/v1/auth/refresh",
+            "/api/v1/auth/nickname/**",
+    };
+
+    private static final String[] SWAGGER_URIS = {
+            "/favicon.ico",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+
+    private static final String[] IMAGE_URIS = {
+            "/api/v1/images/**",
+            "/images/**",
+    };
+
+    private static final String[] UPLOAD_IMAGE_URIS = {
+            "/api/v1/images",
+            "/api/v1/buildings/*/images"
+    };
+
+    private static final String[] ADMIN_URIS = {
+            "/api/v1/admin/**"
+    };
+}
