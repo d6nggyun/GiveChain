afd9dec7a980fb8fe340574e8d1b2a99e84d6617
d6nggyun <d6nggyun@gmail.com>
2025-12-01 02:48:13 +0900

    ✨ feat: 온체인 json 파일 업데이트 - #6

--- frontend/lib/aaSdk.ts
+++ frontend/lib/aaSdk.ts
@@ -6,6 +6,10 @@ import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from "@web3auth/base";
 
 const clientId = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID as string;
 
+const SEPOLIA_RPC_URL =
+  process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL ||
+  "https://rpc.ankr.com/eth_sepolia"; 
+
 let web3auth: Web3Auth | null = null;
 
 export type LoginResult = {
@@ -24,8 +28,12 @@ export const initWeb3Auth = async (): Promise<Web3Auth> => {
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
     chainConfig: {
       chainNamespace: CHAIN_NAMESPACES.EIP155,
-      chainId: "0x1",
-      rpcTarget: "https://rpc.ankr.com/eth",
+      chainId: "0xaa36a7", // 11155111(10진수)
+      rpcTarget: SEPOLIA_RPC_URL,
+      displayName: "Sepolia Testnet",
+      ticker: "ETH",
+      tickerName: "Ethereum",
+      blockExplorer: "https://sepolia.etherscan.io",
     },
   };
 
