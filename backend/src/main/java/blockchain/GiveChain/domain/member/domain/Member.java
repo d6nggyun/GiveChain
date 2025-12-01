849502cc0f861661526bfa1b4258f8098817fffa
d6nggyun <d6nggyun@gmail.com>
2025-12-02 01:56:30 +0900

    🌎 ci: Blue Green 무중단 배포 방식 도입

--- backend/src/main/java/blockchain/GiveChain/domain/member/domain/Member.java
+++ backend/src/main/java/blockchain/GiveChain/domain/member/domain/Member.java
@@ -24,7 +24,7 @@ public class Member extends BaseTimeEntity {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
