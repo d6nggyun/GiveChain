da076eb05c6cd1f4ad3efaaa0106787b3688f817
d6nggyun <d6nggyun@gmail.com>
2025-12-02 03:38:01 +0900

    🔧 chore: Member Email 중복 필터 제거

--- backend/src/main/java/blockchain/GiveChain/domain/member/service/MemberService.java
+++ backend/src/main/java/blockchain/GiveChain/domain/member/service/MemberService.java
@@ -4,8 +4,6 @@ import blockchain.GiveChain.domain.member.domain.Member;
 import blockchain.GiveChain.domain.member.domain.MemberDetail;
 import blockchain.GiveChain.domain.member.dto.req.UpdateAdditionalInfoRequest;
 import blockchain.GiveChain.domain.member.repository.MemberRepository;
-import blockchain.GiveChain.global.exception.CustomException;
-import blockchain.GiveChain.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,10 +19,6 @@ public class MemberService {
     @Transactional
     public void updateAdditionalInfo(MemberDetail userDetails, UpdateAdditionalInfoRequest request) {
         Member member = userDetails.getMember();
-
-        if (memberRepository.existsByEmail(request.email())) {
-            throw new CustomException(ErrorCode.EMAIL_ALREADY_EXISTS);
-        }
 
         member.updateAdditionalInfo(request.name(), request.email(), request.country());
         memberRepository.save(member);
